561d163f00824d336e581d97310c8baeb148b5db
Devramsean0 <81807361+devramsean0@users.noreply.github.com>
2023-01-26 19:36:14 +0000

    Core

---
+++ src/routes/[org].tsx
@@ -0,0 +1,12 @@
+import { Title } from "solid-start";
+
+export default function Home() {
+  return (
+    <>
+      <Title>Home | onHCB</Title>
+      <main>
+        
+      </main>
+    </>
+  );
+}
