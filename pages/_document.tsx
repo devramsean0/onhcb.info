9bbc28d1579e573f72302ed1e89789fd967e0a4a
Devramsean0 <81807361+devramsean0@users.noreply.github.com>
2023-05-30 18:12:33 +0100

    Add a consistent title

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,3 +1,4 @@
+import { StaticImages } from '@/lib/static_images'
 import { Html, Head, Main, NextScript } from 'next/document'
 import React from 'react'
 
@@ -5,6 +6,8 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head />
+      <title>onhcb.info</title>
+      <link rel="icon" href={StaticImages.HCB_LOGO_DARK} />
       <body>
         <Main />
         <NextScript />
