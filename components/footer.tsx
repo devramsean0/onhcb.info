c4262065ae805114e014575098b9c645da51c63c
Devramsean0 <81807361+devramsean0@users.noreply.github.com>
2023-05-31 07:38:02 +0100

    Move location of github

--- components/footer.tsx
+++ components/footer.tsx
@@ -1,5 +1,4 @@
-import Icon from "@hackclub/icons";
-import { Card, Link } from "theme-ui";
+import { Card} from "theme-ui";
 
 export function Footer() {
     const year = new Date().getFullYear();
@@ -8,7 +7,6 @@ export function Footer() {
             <center>
                 <h2>&copy; Sean Outram {year}</h2>
             </center>
-            <Link href="https://github.com/devramsean0/onhcb.info"><Icon glyph="github"/></Link>
         </Card>
     )
 }
--- components/navbar.tsx
+++ components/navbar.tsx
@@ -2,13 +2,15 @@ import { Grid } from "theme-ui";
 import ColorSwitcher from "./colour-switcher";
 import { Search } from "./search";
 import Link from "next/link";
+import Icon from "@hackclub/icons";
 
 export function Navbar() {
     return (
-        <Grid columns={[1, null, 3]}>
+        <Grid columns={[1, null, 4]}>
             <Link href="/"><h1>onhcb.info</h1></Link>
             <Search textboxID="inNavbarSearch" />
             <ColorSwitcher />
+            <Link href="https://github.com/devramsean0/onhcb.info"><Icon glyph="github" size={32} /></Link>
         </Grid>
     )
 }
